1e68371d6197e37b430c863b66ba15d7299f5b1f
ctfang <1992053348@qq.com>
2018-07-24 21:42:49 +0800

    新增调度器

---
+++ game-worker/Support/Middleware/RouterMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 明月有色
+ * Date: 2018/7/24
+ * Time: 21:30
+ */
+
+namespace GameWorker\Support\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RouterMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * Process an incoming server request and return a response, optionally delegating
+     * response creation to a handler.
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $handler->handle($request);
+    }
+}
